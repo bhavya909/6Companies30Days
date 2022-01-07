a810785c8094e0a84167b447fe507f2d9769de26
bhavya909 <65531510+bhavya909@users.noreply.github.com>
2022-01-08 00:19:58 +0530

    Create IPL 2021 - Match Day 2.cpp

---
+++ Amazon/IPL 2021 - Match Day 2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        vector<int> ans;
+        deque<int> q(k);
+        for(int i=0;i<k;i++)
+        {
+            while(!q.empty() && arr[i]>q.back() )
+            {
+                q.pop_back();
+            }
+            q.push_back(arr[i]);
+        }
+        ans.push_back(q.front());
+        for(int i=k;i<n;i++)
+        {
+            if(arr[i-k]==q.front())
+            q.pop_front();
+            
+            while(!q.empty() && arr[i]>q.back() )
+            {
+                q.pop_back();
+            }
+            q.push_back(arr[i]);
+            ans.push_back(q.front());
+        }
+        return ans;
+        
+        
+    }
+};
